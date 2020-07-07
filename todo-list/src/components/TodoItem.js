28cf2111f8d36fd3222553fe2c150134014ce8ec
trinh250899 <trinhctpc1@gmail.com>
2020-07-07 20:23:20 +0700

    initial commit

---
+++ todo-list/src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React , {Component } from 'react';
+
+class TodoItem extends Component {
+    render(){
+        return  <div className ="TodoItem">
+            <p>Hello  </p>
+
+            </div>
+
+           
+        };
+
+    }
+
+export default TodoItem;
